15d8c0a39bc2a049d1c97b1c053cfbf30d325424
linh.tran <linh.tran@trustingsocial.com>
2022-08-11 21:41:36 +0700

    add common scripts

---
+++ vars/log.groovy
@@ -0,0 +1,23 @@
+#!groovy
+
+def info(message) {
+  echo "\u001B[34mINFO: ${message}\u001B[0m"
+}
+
+def warning(message) {
+  ansiColor('xterm') {
+    echo "\u001B[35mWARNING: ${message}\u001B[0m"
+  }
+}
+
+def error(message) {
+  ansiColor('xterm') {
+    echo "\u001B[31mERROR: ${message}\u001B[0m"
+  }
+}
+
+def debug(message) {
+  ansiColor('xterm') {
+    echo "\u001B[36mDEBUG: ${message}\u001B[0m"
+  }
+}
